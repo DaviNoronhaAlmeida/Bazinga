106391d6371d5cb63400fea677d77a55b6016e9d
Davi Noronha <davinoronhaalmeida@gmail.com>
2023-03-05 18:00:05 -0300

    create

---
+++ app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:app/ui/screens/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const LoginPage(),
+        ),
+      ],
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

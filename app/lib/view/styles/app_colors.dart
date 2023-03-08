e2b0719f406178800eba5147cb272086319b1e20
Leonardo de Andrade Vieira <leonardo_andrade_vieira@hotmail.com>
2023-03-07 21:35:01 -0300

    feat: update colors file

--- app/lib/view/styles/app_colors.dart
+++ app/lib/view/styles/app_colors.dart
@@ -4,22 +4,24 @@ import 'package:get/get.dart';
 class AppColors extends GetxController {
   Color textBtnColor = const Color.fromRGBO(255, 255, 255, 1);
   Color redColor = const Color.fromRGBO(228, 64, 64, 1);
-  Color descriptionColor = const Color.fromRGBO(255, 255, 255, 0.6);
 
   static const darkBarColor = Color.fromRGBO(25, 14, 79, 1);
   static const darkBackgroundColor = Color.fromRGBO(3, 1, 44, 1);
   static const darkBgPostsColor = Color.fromRGBO(26, 32, 62, 1);
   static const darkTextColor = Color.fromRGBO(255, 255, 255, 1);
+  static const darkDescriptionColor = Color.fromRGBO(255, 255, 255, 0.6);
 
   static const lightBarColor = Color.fromRGBO(54, 54, 54, 1);
   static const lightBackgroundColor = Color.fromRGBO(245, 245, 245, 1);
   static const lightBgPostsColor = Color.fromRGBO(225, 225, 225, 1);
-  static const lightTextColor = Color.fromARGB(255, 0, 0, 0);
+  static const lightTextColor = Color.fromARGB(0, 0, 0, 1);
+  static const lightDescriptionColor = Color.fromRGBO(0, 0, 0, 0.6);
 
-  dynamic barColor = lightBarColor;
+  dynamic barColor = darkBarColor;
   dynamic backgroundColor = darkBackgroundColor;
   dynamic bgPostsColor = darkBgPostsColor;
   dynamic textColor = darkTextColor;
+  dynamic descriptionColor = darkDescriptionColor;
 
   void change() {
     /* backgroundColor = const Color.fromRGBO(245, 245, 245, 1);

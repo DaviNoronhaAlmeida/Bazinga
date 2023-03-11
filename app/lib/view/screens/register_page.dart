9333e5b1bbcddb8eaf8b45135fea4f3be6674621
Davi Noronha <davinoronhaalmeida@gmail.com>
2023-03-11 18:10:18 -0300

    feat: add register page

---
+++ app/lib/view/screens/register_page.dart
@@ -0,0 +1,62 @@
+import 'package:app/view/widgets/custom_input.dart';
+import 'package:app/view/widgets/custom_small_button.dart';
+import 'package:app/view/widgets/custom_text_button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../styles/app_colors.dart';
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({super.key});
+  final AppColors _appColors = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: _appColors.backgroundColor,
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 90),
+                width: 150,
+                height: 150,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/logo_white.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 42.5),
+                child: CustomInput(inputTittle: 'Nome de usuário:'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 19.5),
+                child: CustomInput(inputTittle: 'Email:'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 19.5),
+                child: CustomInput(inputTittle: 'Senha:'),
+              ),
+              CustomSmallButton(
+                tittleBtn: 'CADASTRAR',
+                customMargin: 70,
+                function: () => Get.toNamed('/'),
+              ),
+              CustomTextButton(
+                tittle: 'VOLTAR',
+                function: () => Get.back(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

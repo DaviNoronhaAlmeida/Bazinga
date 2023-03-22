c3bfd31bad7f7e67040122e1459d63676d4e6e7f
Leonardo de Andrade Vieira <leonardo_andrade_vieira@hotmail.com>
2023-03-22 01:21:05 -0300

    feature/comments-fix-and-updates

--- app/lib/view-model/services/update_name_service.dart
+++ app/lib/view-model/services/update_name_service.dart
@@ -4,8 +4,9 @@ import 'package:app/view/widgets/custom_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../view-model/utils/user_info.dart';
+import '../../view-model/utils/token.dart';
 
-void update(String? token, String req, String param, String newName) async {
+void updateName(String? token, String req, String param, String newName) async {
   Map<String, dynamic> data = await newUpdateReq(token, req);
 
   if (data['status'] == 200) {
@@ -16,14 +17,12 @@ void update(String? token, String req, String param, String newName) async {
         content: Text('$param foi alterado(a) com sucesso!'),
       ),
     );
-
     Get.find<Info>().setName(newName);
+    Get.find<Token>().setToken(data['token']);
 
     // ignore: no_leading_underscores_for_local_identifiers
     final FeedController _feedController = Get.put(FeedController());
     _feedController.loadFeed();
-    /* await Future.delayed(const Duration(seconds: 2));
-    Get.offAllNamed('/settings'); */
   } else {
     CustomSnackBar.show(data['content']['message']);
   }
--- app/lib/view/widgets/custom_icon.dart
+++ app/lib/view/widgets/custom_icon.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../view-model/utils/icon.dart';
 import '../styles/app_colors.dart';
 
 class ChooseIcon extends StatelessWidget {
   ChooseIcon({super.key});
 
   final AppColors _appColors = Get.find();
+  final IconController iconController = Get.put(IconController());
 
   @override
   Widget build(BuildContext context) {
@@ -34,81 +36,135 @@ class ChooseIcon extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(
                             vertical: 30, horizontal: 16),
                         child: Wrap(
-                          spacing: 40.0,
+                          spacing: 10.0,
                           runSpacing: 40.0,
                           children: [
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon1.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon2.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon3.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon4.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon5.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon6.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon7.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon8.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon9.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon10.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon11.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon12.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon13.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon14.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon15.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon16.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon17.png')),
-                            SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Image.asset('assets/images/icon18.png')),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon1.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon2.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon3.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon4.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon5.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon6.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon7.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon8.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon9.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon10.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon11.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon12.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon13.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon14.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon15.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon16.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon17.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
+                            IconButton(
+                              icon: Image.asset('assets/images/icon18.png'),
+                              iconSize: 50,
+                              onPressed: () {
+                                // Modify icon controller here
+                              },
+                            ),
                           ],
                         ),
                       ),
